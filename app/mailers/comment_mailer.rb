033cbf0cfa4ee19ebbf89b96015eba0570b9c4f5
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-05-21 22:51:00 +0900

    contentからusernameを抜き出し取得

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,6 +1,7 @@
 class CommentMailer < ApplicationMailer
-  def mention(user)
-    @user = user
-    mail to: user.email, subject: '【お知らせ】メンションされました'
+  def new_mention(mention_user_ids)
+    users = User.where(id: mention_user_ids)
+    mail to: users.pluck(:email), subject: '【お知らせ】メンションされました'
+    binding.pry
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -28,20 +28,53 @@ class Comment < ApplicationRecord
   #   binding.pry
   # end
 
+  # def has_username?
+  #   username = User.pluck(:username)
+  #   # 全てのユーザーの中からusernameカラムの値のみを配列で取得
+  #   if username.any? { |username| content.include?("@#{username}") }
+  #   # コメント内に"@"+"username"が含まれているかどうかを検索
+  #     puts mention_user
+  #     binding.pry
+  #   end
+  # end
+
   def has_username?
     username = User.pluck(:username)
     # 全てのユーザーの中からusernameカラムの値のみを配列で取得
     username.any? { |username| content.include?("@#{username}") }
     # コメント内に"@"+"username"が含まれているかどうかを検索
-    # binding.pry
   end
 
   after_create :send_email, if: :has_username?
+  # binding.pry
 
+  # def get_mention_user(content)
+  #   content.match(/@/)
+  #   content_username = $'
+  #   mention_user = User.where(username: "#{content_username}")
+  # end
+
+  # def get_mention_user
+  #   mention = content.scan(/@\w+/)
+  #   # content内から"@"+"username"を配列で抜き出す
+  #   mention_username = mention.join(",").scan(/\w+/)
+  #   # mentionを配列から文字列に変換し、@以外を抜き出す
+  #   mention_user = User.where(username: mention_username)
+  #   # ユーザーの中からusrenameキーを指定し、mentionus_rnameに該当するユーザーを探す
+  #   mention_user.map(&:id)
+  #   # そのままユーザーを渡せないのでmapを使ってidに変換
+  # end
 
   private
-
   def send_email
-    CommentMailer.mention(user).deliver_later
+    mention = content.scan(/@\w+/)
+    # content内から"@"+"username"を配列で抜き出す
+    mention_username = mention.join(",").scan(/\w+/)
+    # mentionを配列から文字列に変換し、@以外を抜き出す
+    mention_user = User.where(username: mention_username)
+    # ユーザーの中からusrenameキーを指定し、mentionus_rnameに該当するユーザーを探す
+    mention_user_ids = mention_user.map(&:id)
+    # そのままユーザーを渡せないのでmapを使ってidに変換
+    CommentMailer.new_mention(mention_user_ids).deliver_later
   end
 end
