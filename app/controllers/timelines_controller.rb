368a10d6b5a7801796377980c398a3f599e1fc4b
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-06-07 22:49:07 +0900

    24時間以内に作成された投稿の中で「いいね」が多い投稿を5つ表示するタイムラインページを作成

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,7 +2,7 @@ class TimelinesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    user_ids = current_user.followings.pluck(:id)
-    @articles = Article.where(user_id: user_ids).order(created_at: :desc)
+    @articles = Article.joins(:likes).where(created_at: 24.hours.ago..).group(:id).order(created_at: :desc).order('COUNT(likes.id)').limit(5)
+    # likesテーブルを左外部結合、24時間以内の投稿を検索、グループを指定、新着順に並び替え、いねの多い5件を取得
   end
 end
