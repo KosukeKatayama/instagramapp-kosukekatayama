ed895c557f498c300c190b02171f03362ee1f464
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-06-16 21:36:09 +0900

    Userモデル パスワード、ユーザーネームの文字数の制限に関するテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,4 +71,40 @@ RSpec.describe User, type: :model do
       expect(user2.errors.messages[:username][0]).to eq('はすでに存在します')
     end
   end
+
+  context 'パスワードの長さが6文字以下の場合' do
+    let!(:user) { build(:user, password: Faker::Lorem.characters(number: 5))}
+
+    before do
+      user.save
+    end
+
+    it 'ユーザーの登録に失敗する' do
+      expect(user.errors.messages[:password][0]).to eq('は6文字以上で入力してください')
+    end
+  end
+
+  context 'パスワードの長さが20文字以上の場合' do
+    let!(:user) { build(:user, password: Faker::Lorem.characters(number: 21))}
+
+    before do
+      user.save
+    end
+
+    it 'ユーザーの登録に失敗する' do
+      expect(user.errors.messages[:password][0]).to eq('は20文字以内で入力してください')
+    end
+  end
+
+  context 'ユーザーネームの長さが30文字以上の場合' do
+    let!(:user) { build(:user, username: Faker::Lorem.characters(number: 31))}
+
+    before do
+      user.save
+    end
+
+    it 'ユーザーの登録に失敗する' do
+      expect(user.errors.messages[:username][0]).to eq('は30文字以内で入力してください')
+    end
+  end
 end
