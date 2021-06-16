1fa6dd3b8e9205de087fc2791a027e7afffefe81
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-06-16 20:16:40 +0900

    Commentモデル contentが2200文字以上の場合、コメントの送信に失敗するかどうかのテスト

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let!(:user) { create(:user) }
+  let!(:article) { create(:article, user: user) }
+  
+  context 'contentが2000文字以上の場合' do
+    let!(:comment) { build(:comment, content: Faker::Lorem.characters(number: 2500), user: user, article: article) }
+
+    before do
+      comment.save
+    end
+
+    it 'コメントの送信に失敗する' do
+      expect(comment.errors.messages[:content][0]).to eq('は2000文字以内で入力してください')
+    end
+  end
+end
