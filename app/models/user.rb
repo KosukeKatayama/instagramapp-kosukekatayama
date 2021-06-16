7fe4fe1d8c73420f6686a3e449549b7a1356ea8e
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-06-16 13:32:16 +0900

    email, usernameに対してのvalidationを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,11 @@ class User < ApplicationRecord
   has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
   has_many :followers, through: :follower_relationships, source: :follower
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :username, presence: true, uniqueness: true
+
+
   def prepare_profile
     profile || build_profile
   end
