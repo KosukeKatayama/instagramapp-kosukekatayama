f7ce2ad97e3af90d0e74caa3f6d4daaa23a9e465
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-06-23 10:07:16 +0900

    like-post

--- spec/requests/api/likes_spec.rb
+++ spec/requests/api/likes_spec.rb
@@ -16,4 +16,19 @@ RSpec.describe 'Api::Likes', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'POST /api/likes' do
+    let!(:like) { create(:like, article: article, user: user) }
+
+    it 'JSON形式で送信される' do
+      post api_like_path(article_id: article.id, format: :json)
+      expect(response.content_type).to eq 'application/json; charset=utf-8'
+    end
+
+    it '正常にいいねできる' do
+      post api_like_path(article_id: article.id, format: :json)
+      expect(response).to have_http_status(200)
+      # expect(Like.count).to eq 1
+    end
+  end
 end
