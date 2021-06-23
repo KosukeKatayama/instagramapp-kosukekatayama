4b7af2b6bf96827c44ec66a7daf5d86d3d93c3ff
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-06-23 10:06:33 +0900

    like-get

---
+++ spec/requests/api/likes_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::Likes', type: :request do
+  let!(:user) { create(:user) }
+  let!(:article) { create(:article, user: user) }
+
+  before do
+    sign_in user
+  end
+
+  describe 'GET /api/likes' do
+    let!(:like) { create(:like, article: article, user: user) }
+
+    it '200 Statos' do
+      get api_like_path(article_id: article.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+end
