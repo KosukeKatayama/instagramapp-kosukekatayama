e426b90e553e086c379d1c3550572f5a2d391ae1
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-06-23 10:07:38 +0900

    like-delete

--- spec/requests/api/likes_spec.rb
+++ spec/requests/api/likes_spec.rb
@@ -31,4 +31,14 @@ RSpec.describe 'Api::Likes', type: :request do
       # expect(Like.count).to eq 1
     end
   end
+
+  describe 'DELETE /api/likes' do
+    let!(:like) { create(:like, article: article, user: user) }
+
+    it '200 Statos' do
+      delete api_like_path(article_id: article.id)
+      expect(response).to have_http_status(200)
+      expect(Like.count).to eq 0
+    end
+  end
 end
