e120cc2dc9a7dc4947844e9587185afec10b468e
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-06-27 01:36:32 +0900

    articles-post

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -18,4 +18,29 @@ RSpec.describe 'Articles', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'POST /articles' do
+    let!(:user) { create(:user) }
+    let!(:article) { create_list(:article, 10, user: user) }
+
+    context 'ログインしていた場合' do
+      before do
+        sign_in user
+      end
+      it '新規投稿ができる' do
+        article_params = attributes_for(:article)
+        post articles_path({article: article_params})
+        expect(response).to have_http_status(302)
+        expect(Article.last.content).to eq(article_params[:content])
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'ログイン画面に遷移する' do
+        article_params = attributes_for(:article)
+        post articles_path({article: article_params})
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
